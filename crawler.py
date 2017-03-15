cd0bc512dd6e2a3876573ba0a74f7fe7a8354c75
bram <bram.boomen@gmail.com>
2017-03-15 11:54:56 +0100

    Refactoring crawler

--- crawler.py
+++ crawler.py
@@ -8,48 +8,48 @@ bp = "https://www.brainpickings.org/2017/03/01/mary-ruefle-madness-rack-and-hone
 
 def bptest():
     print("visiting: " + bp)
-    links = getLinks(bp)
-    # links = bp_afterprocessor(links)
+    crawler = Crawler()
+    links = crawler.get_internal_links(bp)
     for l in links:
         print(l)
 
 
-# Main Crawler function
-def getLinks(url):
-    print("started")
-    links = []
-    baseUrl = url
-    response = urlopen(url)
-    if response.getheader('Content-Type') == "text/html; charset=UTF-8": # Make sure that page is HTML
-        htmlBytes = response.read()
-        htmlString = htmlBytes.decode("utf-8")
-        entry = bp_preprocessor(htmlString)
-        print("crawling " + url)
-        links = findurls(entry)
+class Crawler:
+
+    def get_links(self, url):
+        html = self.read_url(url)
+        print("started")
+        entry = self.bp_preprocessor(html)
+
+        links = []
+        for link in entry.find_all(href=True):
+            links.append(link['href'])
         return links
-    else:
-        print(url + "not crawlable")
-        return "",[]
+
+    def get_internal_links(self, url):
+        links = self.get_links(url)
+        internal_links = []
+        for link in links:
+            if "brainpickings.org/20" in link and link not in internal_links:
+                internal_links.append(link)
+
+        return internal_links
 
 
-def bp_preprocessor(html):
-    soup = BeautifulSoup(html, "html.parser")
-    r = soup.find('div', { "class" : "entry_content" })
 
-    return r
-
-
-def findurls(html):
-    links = []
-    for link in html.find_all(href=True):
-        links.append(link['href'])
-    return links
+    def read_url(self, url):
+        response = urlopen(url)
+        if response.getheader('Content-Type') == "text/html; charset=UTF-8":  # Make sure that page is HTML
+            htmlBytes = response.read()
+            htmlString = htmlBytes.decode("utf-8")
+        else:
+            print(url + "not crawlable")
+            return "", []
+        return htmlString
 
 
-def bp_afterprocessor(links):
-    newlinks = []
-    for link in links:
-        if "brainpickings.org/20" in link and link not in newlinks:
-            newlinks.append(link)
+    def bp_preprocessor(self, html):
+        soup = BeautifulSoup(html, "html.parser")
+        r = soup.find('div', { "class" : "entry_content" })
 
-    return newlinks
+        return r
