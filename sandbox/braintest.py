9b052a0b00b6162dc1be5560d0b6f2c6301e1b2e
bram <bram.boomen@gmail.com>
2017-03-24 17:01:35 +0100

    New test method

--- sandbox/braintest.py
+++ sandbox/braintest.py
@@ -8,18 +8,16 @@ import sys
 bp = "https://www.brainpickings.org/2017/03/01/mary-ruefle-madness-rack-and-honey-prayer/"
 
 
-def crawltest():
-    print("visiting: " + bp)
-    crawler = Crawler()
-    links = crawler.get_internal_links(bp)
-    for l in links:
-        print(l)
+def crawl_test():
+    local = True
+    save = False
+    crawl(local, save)
 
 
-def indextest():
+def index_test():
     indexer = Indexer(2017, 3, 21, local=False, save=True)
     indexer.bp_index()
 
 
-# crawltest()
-indextest()
+crawl_test()
+# index_test()
